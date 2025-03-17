d2bcec4625696cfb8410f54c3eedd5052c32d55d
computermouth <computermouth@crunchbangplusplus.org>
2025-03-17 17:02:45 -0500

    bytes annotation working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use msgpacker::prelude::*;
 use std::io::{self, Write};
 use serde::{Deserialize, Serialize};
-use rmp_serde::{Deserializer, Serializer};
-
-const SOME: u32 = 10;
+use rmp_serde::Serializer;
 
 #[derive(MsgPacker)]
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
@@ -16,7 +14,7 @@ pub struct Payload {
   // ii16: i16,
   // ii32: i32,
   // ii64: i64,
-  vu08: Vec<u8 >,
+  // vu08: Vec<u8 >,
   // vu16: Vec<u16>,
   // vu32: Vec<u32>,
   // vu64: Vec<u64>,
@@ -24,23 +22,27 @@ pub struct Payload {
   // vi16: Vec<i16>,
   // vi32: Vec<i32>,
   // vi64: Vec<i64>,
+  #[msgpacker(binary)]
+  #[serde(with = "serde_bytes")]
+  bb: Vec<u8>,
   // uu8struct: Uu8Struct,
   // uu8tuplestruct: Uu8TupleStruct,
   // unitstruct: UnitStruct,
+  // vempty: Vec<u8>,
 }
 
 #[derive(MsgPacker)]
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
+// #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct Uu8Struct {
   first: u8,
 }
 
 #[derive(MsgPacker)]
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
+// #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct Uu8TupleStruct(u8);
 
 #[derive(MsgPacker)]
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
+// #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct UnitStruct;
 
 fn main() {
@@ -55,7 +57,7 @@ fn main() {
       // ii16: 0,
       // ii32: 0,
       // ii64: 0,
-      vu08: vec![0],
+      // vu08: vec![0],
       // vu16: vec![0],
       // vu32: vec![0],
       // vu64: vec![0],
@@ -63,13 +65,15 @@ fn main() {
       // vi16: vec![0],
       // vi32: vec![0],
       // vi64: vec![0],
+      bb: vec![1,2,3,4],
       // uu8struct: Uu8Struct { first: 0 },
       // uu8tuplestruct: Uu8TupleStruct(0),
       // unitstruct: UnitStruct,
+      // vempty: vec![],
     };
 
-    // out.pack(&mut buf);
-    out.serialize(&mut Serializer::new(&mut buf)).unwrap();
+    out.pack(&mut buf);
+    // out.serialize(&mut Serializer::new(&mut buf)).unwrap();
 
     let stdout = io::stdout();
     let mut handle = stdout.lock();
